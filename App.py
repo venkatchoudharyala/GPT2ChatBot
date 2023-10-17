6508175b5b59f48c51324792ba9750b2db9ef288
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2023-10-17 21:10:51 +0530

    Update App.py

--- App.py
+++ App.py
@@ -19,30 +19,30 @@ if "user" in st.session_state:
 
 else:
 	st.session_state["LoginVal"] = False
-
-prompts = ["Write a story about a robot who falls in love with a human.",
-           "Write a poem about the beauty of nature.",
-           "Write a letter to your future self.",
-           "Write a news article about a recent scientific discovery.",
-           "Write a song about your favorite animal.",
-           "Write a script for a short film.",
-           "Write a blog post about the importance of recycling.",
-           "Write a marketing copy for a new product.",
-           "Write a technical document for a new software feature.",
-           "Write a creative story about a world where everyone has superpowers.",
-           "Write a horror story about a haunted house.",
-           "Write a mystery story about a missing person.",
-           "Write a science fiction story about a journey to another planet."]
-
-selected_prompt = st.selectbox("Select a prompt:", prompts)
-Uprompt = st.chat_input("Say Something")
-
-if selected_prompt and st.checkbox("Use Above Prompmts", value = True):
-	Uprompt = selected_prompt
-prompt = "For ur Specific Query " + Uprompt + " you have to"
-model = AutoModelForCausalLM.from_pretrained("gpt2")
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
-prompter = pipeline("text-generation", model = model, tokenizer = tokenizer, max_new_tokens = 500)
-MachineOP = prompter(prompt)
-k = st.chat_message("assistant")
-k.write(MachineOP[0]['generated_text'])
+if st.session_state["LoginVal"]:
+	prompts = ["Write a story about a robot who falls in love with a human.",
+	           "Write a poem about the beauty of nature.",
+	           "Write a letter to your future self.",
+	           "Write a news article about a recent scientific discovery.",
+	           "Write a song about your favorite animal.",
+	           "Write a script for a short film.",
+	           "Write a blog post about the importance of recycling.",
+	           "Write a marketing copy for a new product.",
+	           "Write a technical document for a new software feature.",
+	           "Write a creative story about a world where everyone has superpowers.",
+	           "Write a horror story about a haunted house.",
+	           "Write a mystery story about a missing person.",
+	           "Write a science fiction story about a journey to another planet."]
+	
+	selected_prompt = st.selectbox("Select a prompt:", prompts)
+	Uprompt = st.chat_input("Say Something")
+	
+	if selected_prompt and st.checkbox("Use Above Prompmts", value = True):
+		Uprompt = selected_prompt
+	prompt = "For ur Specific Query " + Uprompt + " you have to"
+	model = AutoModelForCausalLM.from_pretrained("gpt2")
+	tokenizer = AutoTokenizer.from_pretrained("gpt2")
+	prompter = pipeline("text-generation", model = model, tokenizer = tokenizer, max_new_tokens = 500)
+	MachineOP = prompter(prompt)
+	k = st.chat_message("assistant")
+	k.write(MachineOP[0]['generated_text'])
