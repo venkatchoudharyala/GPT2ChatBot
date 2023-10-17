44ced32bb11fb1cb9b6dd13b48f443d8063fa7ac
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2023-10-17 19:44:19 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,15 @@
 import streamlit as st
 from transformers import AutoTokenizer, pipeline, AutoModelForCausalLM
 
+hide_st_style = """
+		<style>
+		header {visibility: hidden;}
+		footer {visibility: hidden;}
+  	</style>
+  	"""
+
+st.markdown(hide_st_style, unsafe_allow_html = True)
+
 prompt = st.input("Say Something")
 
 model = AutoModelForCausalLM.from_pretrained("gpt2")
